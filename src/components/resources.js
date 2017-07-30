2152f7643210559e7bc2ec70449f397a2b63fe33
Peter Szujewski <pszujewski@gmail.com>
2017-07-30 17:05:33 -0400

    Working on Section 4 HOC, next is lecture 39 Action Creator Hookup

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Resources(props) {
+  return (
+    <div>
+      Super Special Recipe
+      <ul>
+        <li>1 cup sugar</li>
+        <li>1 cup pepper</li>
+        <li>1 cup salt</li>
+        <li>Happiness and optimism</li>
+      </ul>
+    </div>
+  );
+}
